b249c2bafc7b1ffeac2561185f0fd6da72e5c816
Kishore N A <kkish056@gmail.com>
2024-08-31 15:56:14 +0530

    Removed and Updated Migration Files

--- LearningDesignPattern/Migrations/20240831102355_InitialCreate.cs
+++ LearningDesignPattern/Migrations/20240831102355_InitialCreate.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -18,7 +19,8 @@ namespace LearningDesignPattern.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Quantity = table.Column<int>(type: "int", nullable: false),
-                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
                 },
                 constraints: table =>
                 {
